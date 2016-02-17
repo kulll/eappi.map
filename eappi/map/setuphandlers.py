1c37c6499b1750d0ee13dc97728420e59cd93f32
glenn <glenn@glenn-Lenovo-G480>
2016-02-17 12:38:00 +0800

    register settings

--- eappi/map/setuphandlers.py
+++ eappi/map/setuphandlers.py
@@ -1,5 +1,8 @@
 from collective.grok import gs
 from eappi.map import MessageFactory as _
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from eappi.map.interfaces import IEappiSettings
 
 @gs.importstep(
     name=u'eappi.map', 
@@ -9,5 +12,7 @@ def setupVarious(context):
     if context.readDataFile('eappi.map.marker.txt') is None:
         return
     portal = context.getSite()
+    registry = getUtility(IRegistry)
+    registry.registerInterface(IEappiSettings)
 
     # do anything here
