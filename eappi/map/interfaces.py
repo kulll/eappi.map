1e8d59dcd302691c23d408bb0914c418d27945d5
glenn <glenn@glenn-Lenovo-G480>
2016-02-17 12:35:42 +0800

    add open map quest field

--- eappi/map/interfaces.py
+++ eappi/map/interfaces.py
@@ -1,5 +1,13 @@
 from zope.interface import Interface
+from zope import schema
 
 class IProductSpecific(Interface):
     pass
+
+class IEappiSettings(Interface):
+    
+    openmapquest_api_key = schema.TextLine(
+        title=u'Open Map Quest API Key',
+        default=u''
+    )
 
